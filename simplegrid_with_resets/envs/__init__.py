f216930a84ea8f94184157ade5c197b316bc697d
Jello Zhou <54515304+jellozhou@users.noreply.github.com>
2025-01-26 14:59:09 -0500

    Add files via upload

--- simplegrid_with_resets/envs/__init__.py
+++ simplegrid_with_resets/envs/__init__.py
@@ -1,3 +1,4 @@
 from simplegrid_with_resets.envs.simple_grid import SimpleGridEnv
 from simplegrid_with_resets.envs.simple_grid_resets import SimpleGridEnvResets
 from simplegrid_with_resets.envs.simple_grid_resets_pbc import SimpleGridEnvResetsPBC
+from simplegrid_with_resets.envs.simple_line_resets import SimpleLineEnvResets
